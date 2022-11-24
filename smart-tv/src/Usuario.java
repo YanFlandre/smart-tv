6e10ad01d88d6ea3a9414af6d293bec68e763d3b
YanFlandre <jp_felippe@hotmail.com>
2022-11-24 18:28:16 -0300

    Atualizacão projeto

---
+++ smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTV smartTV = new SmartTV();
+
+        smartTV.diminuirVolume ();
+        smartTV.diminuirVolume ();
+        smartTV.diminuirVolume ();
+        smartTV.aumentarVolume ();
+        System.out.println("Volume Atual : " + smartTV.volume);
+
+        System.out.println("TV Ligada : " + smartTV.ligada);
+        System.out.println("Canal Atual : " + smartTV.canal);
+        System.out.println("Volume Atual : " + smartTV.volume);
+
+        smartTV.ligar ();
+        System.out.println("Novo Status -> TV Ligada : " + smartTV.ligada);
+        
+        smartTV.desligar ();
+        System.out.println("Novo Status -> TV Ligada : " + smartTV.ligada);
+
+        smartTV.mudarCanal(13);
+
+    
+       
+
+
+    }
+}
